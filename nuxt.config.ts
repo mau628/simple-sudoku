a493d2e61f6610b1292e0ba94e7dc5252a7f4800
Jorge Gonzalez <mau628@gmail.com>
2026-01-09 16:03:50 -0600

    Add title template for app head in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      titleTemplate: 'Simple Sudoku',
+    }
+  },
   compatibilityDate: '2025-07-15',
   css: [
     'bulma',
